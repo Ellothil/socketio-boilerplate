2231d9c8d7c4ac076f2d776aa54bf33d9e7aad71
Ellothil <nreith4@gmail.com>
2025-09-24 01:45:21 +0200

    clean config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		allowedHosts: ["tunnelmole.net", "*.tunnelmole.net"],
-		proxy: {
-			// Proxy /socket.io requests to our NestJS server
-			"/socket.io": {
-				target: "http://localhost:3000", // Your NestJS server address
-				ws: true, // Important for websockets
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy /socket.io requests to our NestJS server
+      "/socket.io": {
+        target: "http://localhost:3000", // Your NestJS server address
+        ws: true, // Important for websockets
+      },
+    },
+  },
 });
