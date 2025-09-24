f605473f63011b2e5595a35967482956aa3a1d9c
Ellothil <nreith4@gmail.com>
2025-09-24 12:59:24 +0200

    ahhhh

--- node_modules/.pnpm/node_modules/client/src/socket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client';
-
-// During development, the Vite proxy will handle this.
-// In production, the server will serve the client, so the origin is the same.
-export const socket = io();
